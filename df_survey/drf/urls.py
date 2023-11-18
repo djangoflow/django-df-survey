f065c156ecdc428a6f2896a9c8409f5f799d2569
Alexis Yushin <alexis@apexive.com>
2023-11-18 08:55:01 -0400

    refactor stage 1

--- df_survey/drf/urls.py
+++ df_survey/drf/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from .viewsets import UserSurveyViewSet
+from .viewsets import SurveyViewSet
 
 urlpatterns = []
 
@@ -10,6 +10,6 @@ if settings.DEBUG:
 else:
     router = SimpleRouter()
 
-router.register("user_surveys", UserSurveyViewSet, basename="user_surveys")
+router.register("user_surveys", SurveyViewSet, basename="user_surveys")
 
 urlpatterns += router.urls
