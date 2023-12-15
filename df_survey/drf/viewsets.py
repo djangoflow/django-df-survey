80bf09b255fdce8a0f1ef088a03f9f5d578c73b3
eugapx <111432881+eugapx@users.noreply.github.com>
2023-12-15 22:21:53 +0200

    add get-or-create for UserSurveyViewSet

--- df_survey/drf/viewsets.py
+++ df_survey/drf/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import GenericViewSet
 
 from ..models import UserSurvey
@@ -24,3 +25,18 @@ class UserSurveyViewSet(
         if self.action == "list":
             return UserSurveySerializer
         return self.serializer_class
+
+    def perform_update(self, serializer):
+        if serializer.instance.result:
+            raise ValidationError("Your submission has already been recorded.")
+        return super().perform_update(serializer)
+
+    def get_object(self):
+        pk = self.kwargs.get("pk", "")
+        if pk.startswith("@"):
+            survey_pk = pk[1:]
+            return UserSurvey.objects.get_or_create(
+                user=self.request.user,
+                survey_id=survey_pk,
+            )[0]
+        return super().get_object()
